5761b898ffedd3dd284c891e31011775213e70d9
Marc Kruse <kruse@lanokata.de>
2015-03-12 00:35:10 +0100

    last and penultimate doesn't take ownership of list anymore

--- src/lists/problem02.rs
+++ src/lists/problem02.rs
@@ -1,16 +1,16 @@
 use collections::{ List, Cons, Nil };
 
 // Find the last but one element of a list
-pub fn penultimate<T>(list: List<T>) -> Option<T> 
+pub fn penultimate<T>(list: &List<T>) -> Option<&T> 
 {
-    match list {
+    match *list {
         Nil => return None,
-        Cons(head, tail) => match *tail {
+        Cons(ref head, ref tail) => match **tail {
             Nil => return None,
-            Cons(_, tail_of_tail) => match *tail_of_tail {
+            Cons(_, ref tail_of_tail) => match **tail_of_tail {
                 Nil => return Some(head),
-                Cons(_, _) => return penultimate(*tail_of_tail)
-            }
+                Cons(_, _) => return penultimate(tail)
+            } 
         }
     }
 }
@@ -18,17 +18,23 @@ pub fn penultimate<T>(list: List<T>) -> Option<T>
 #[test]
 fn penultimate_returns_last_but_one_element()
 {
-    assert_eq!(Some(5), penultimate(list![1, 1, 2, 3, 5, 8]));
+    assert_eq!(Some(&5), penultimate(&list![1, 1, 2, 3, 5, 8]));
+}
+
+#[test]
+fn penultimate_returns_last_but_one_element2()
+{
+    assert_eq!(Some(&5), penultimate(&list![1, 1, 2, 3, 4, 5, 8]));
 }
 
 #[test]
 fn penultimate_returns_none_for_empty_list()
 {
-    assert_eq!(None, penultimate::<i32>(Nil));
+    assert_eq!(None, penultimate::<i32>(&Nil));
 }
 
 #[test]
 fn penultimate_returns_none_for_list_with_only_one_element()
 {
-    assert_eq!(None, penultimate(list![1]));
+    assert_eq!(None, penultimate(&list![1]));
 }
